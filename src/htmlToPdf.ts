7ac47077ce3e93b5814346172db4630e81fbfd1f
Freek Bes <freekbladnl@gmail.com>
2022-04-06 14:54:04 +0200

    feat: add pdf conversion

---
+++ src/htmlToPdf.ts
@@ -0,0 +1,13 @@
+import puppeteer from 'puppeteer'
+
+export async function htmlToPdf(htmlFilePath: string, outputFilePath: string) {
+	const browser = await puppeteer.launch({ headless: true })
+	const page = await browser.newPage()
+	await page.goto(`file://${__dirname}/../${htmlFilePath}`, { waitUntil: 'networkidle0' })
+	const pdf = await page.pdf({
+		path: outputFilePath,
+		format: 'a4'
+	})
+	await browser.close()
+	return pdf
+}
